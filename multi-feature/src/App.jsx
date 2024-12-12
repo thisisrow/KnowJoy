db356aed66bc553190745eacf0319dbd33f6c864
thisisrow <prathameshmishra2020@gmail.com>
2024-12-12 15:33:04 +0530

    update

--- multi-feature/src/App.jsx
+++ multi-feature/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route,useNavigate } from 'react-router-dom'
 import Home from './Component/Home'
+import ChuckNorrisJoke from './Component/ChuckNorrisJoke'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <p>vjhvkvkv</p>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/ChuckNorrisJoke" element={<Home />} />
+      <Route path="/ChuckNorrisJoke" element={<ChuckNorrisJoke />} />
     </Routes>
     </BrowserRouter>
       
